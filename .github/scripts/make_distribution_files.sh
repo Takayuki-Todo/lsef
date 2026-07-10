fbe0efa8f459b1894a6259164dff83323acac73e
Takayuki Todo <i2686172@cc.kyoto-su.ac.jp>
2026-07-10 11:43:38 +0900

    Expand release packaging targets

--- .github/scripts/make_distribution_files.sh
+++ .github/scripts/make_distribution_files.sh
@@ -1,8 +1,24 @@
-#! /bin/sh
+#!/bin/sh
+set -eu
+
 TAG=$1
 PRODUCT_NAME=lsef
-RELEASE=$PRODUCT_NAME-$TAG-arm64-darwin
-cargo build --release
-mkdir -p dist/$RELEASE
-cp LICENSE README.md target/release/lsef dist/$RELEASE
-tar cvfz dist/$RELEASE.tar.gz -C dist $RELEASE
+TARGET=${2:-${TARGET:-$(rustc -Vv | awk '/^host: / { print $2; exit }')}}
+RELEASE=$PRODUCT_NAME-$TAG-$TARGET
+
+case "$TARGET" in
+	*windows-msvc|*windows-gnu)
+		BIN_NAME=$PRODUCT_NAME.exe
+		;;
+	*)
+		BIN_NAME=$PRODUCT_NAME
+		;;
+esac
+
+cargo build --release --target "$TARGET"
+
+mkdir -p "dist/$RELEASE"
+cp LICENSE README.md README.ja.md "dist/$RELEASE"
+cp -R completions docs "dist/$RELEASE"
+cp "target/$TARGET/release/$BIN_NAME" "dist/$RELEASE/"
+tar cvfz "dist/$RELEASE.tar.gz" -C dist "$RELEASE"
